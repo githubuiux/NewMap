777f621cfb1710173365a455aea625b439e284d3
githubuiux <uiuxista@gmail.com>
2023-06-23 15:39:53 +0300

    Initial Commit

---
+++ NewMap/NewMapApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewMapApp.swift
+//  NewMap
+//
+//  Created by Levent on 23.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct NewMapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
